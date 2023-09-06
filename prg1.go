50bd26394093be12aa53b523abdaaf0b226cfbee
riggod <jaymawkar88@gmail.com>
2023-09-06 23:15:03 +0530

    Update prg1.go
    Go revision

--- prg1.go
+++ prg1.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func main() {
+	var sname, college, div string
+	var rno int
+
+	fmt.Printf("Enter Student name:")
+	fmt.Scanln(&sname)
+	fmt.Printf("Enter division:")
+	fmt.Scanln(&div)
+	fmt.Printf("Enter college name:")
+	fmt.Scanln(&college)
+	fmt.Printf("Enter Student roll no:")
+	fmt.Scanln(&rno)
+
+	fmt.Printf("Student name:%s\nDivision:%s\nCollege name:%s\nRoll no:%d", sname, div, college, rno)
+}
